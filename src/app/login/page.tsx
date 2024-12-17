d64b5f4cbbf6a306f041633ed4f3db38da5ed070
Heriberto Santana <heriberto.santana@dci-student.org>
2024-12-17 09:30:25 +0100

    ok

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,7 +8,7 @@ const Login = () => {
 
   const handleLogin = async () => {
     try {
-      const response = await api.post('/users/login', { email, password });
+      const response = await api.post('api/users/login', { email, password });
       console.log('Login exitoso:', response.data);
     } catch (error) {
       console.error('Error de autenticación:', error);
