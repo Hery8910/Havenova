281fc6c4750e428117f4243fe0cd9cf90df5b684
Heriberto <herysantana89@gmail.com>
2025-03-25 14:02:37 +0100

    About

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,32 +1,21 @@
-import { GetStaticProps } from 'next';
-import Head from 'next/head';
 import api from '../../services/api';
 import BlogList from '../../components/blog/blogList/page';
 import { BlogPost } from '../../types/blog';
 
-interface HomeProps {
-  blogs: BlogPost[];
-}
+export default async function BlogPage() {
+  let blogs: BlogPost[] = [];
 
-const Blog = ({ blogs }: HomeProps) => {
+  try {
+    const { data } = await api.get<BlogPost[]>('/blogs');
+    blogs = data;
+  } catch (error) {
+    console.error('Error fetching blog posts:', error);
+  }
+
   return (
-    <>
-      <Head>
-        <title>Havenova Blog</title>
-        <meta name="description" content="Read expert advice on home maintenance and services." />
-      </Head>
-      <main>
-        <h1>Latest Blog Posts</h1>
-        <BlogList blogs={blogs} />
-      </main>
-    </>
+    <main>
+      <h1>Latest Blog Posts</h1>
+      <BlogList blogs={blogs} />
+    </main>
   );
-};
-
-export const getStaticProps: GetStaticProps = async () => {
-  const { data } = await api.get<BlogPost[]>('/blogs');
-  return { props: { blogs: data }, revalidate: 60 };
-};
-
-
-export default Blog;
+}
