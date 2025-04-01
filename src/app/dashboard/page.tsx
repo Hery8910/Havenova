1a3d01558cc8910184fa0c3c5c2267bc730a7ec8
Heriberto <herysantana89@gmail.com>
2025-04-01 14:40:26 +0200

    Button

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import styles from './page.module.css';
+
+ const Dashboard = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Furniture Assembly</h1>
+
+    </div>
+  );
+};
+
+export default Dashboard
